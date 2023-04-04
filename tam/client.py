6cfb82e477adc348aa848be6075c1acaaedc5697
jwc20 <choij2015@gmail.com>
2023-04-03 17:40:53 -0700

    Authenticate

--- tam/client.py
+++ tam/client.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
- 
 
-import asyncio 
-import websockets 
+
+import asyncio
+import websockets
+
 
 async def chat_client():
     uri = "wss://irc-ws.chat.twitch.tv:443"
 
-    # TODO: Authenticate 
+    # TODO: Authenticate
+    await websocket.send("PASS oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    await websocket.send("NICK xxxxxxxx")
+    await websocket.send("JOIN #xxxxxxxxx")
 
-    while True: 
+    while True:
         message = await websocket.recv()
-        if "PRIVMSG" in message: 
+        if "PRIVMSG" in message:
             parts = message.split(":")
-            # username 
+            # username
             # message
 
             print(part[1], part[2])
