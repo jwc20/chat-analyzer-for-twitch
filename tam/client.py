93f8a12d2dc69939ba26a3c8bf86f475d3485851
jwc20 <choij2015@gmail.com>
2023-04-05 14:09:03 -0700

    Update with PyQt6 client for receiving chat.

--- tam/client.py
+++ tam/client.py
@@ -1,12 +1,20 @@
+import sys
 import asyncio
 import json
 import aiohttp
 import websockets
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLineEdit
+from PyQt6.QtCore import QThread, pyqtSignal, pyqtSlot
+from collections import deque
 
-# Set your credentials
-CLIENT_ID = "your_client_id"
-CLIENT_SECRET = "your_client_secret"
-CHANNEL_NAME = "target_channel_name"
+# Set credentials
+# CLIENT_ID = 'your_client_id'
+# CLIENT_SECRET = 'your_client_secret'
+# CHANNEL_NAME = 'target_channel_name' # Must be lowercase. (e.g. 'theprimeagen')
+
+MAX_MESSAGES = 100
+messages_queue = deque(maxlen=MAX_MESSAGES)
+
 
 async def get_oauth_token(client_id, client_secret):
     url = f"https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials"
@@ -25,22 +33,93 @@ async def get_channel_id(channel_name, token):
             return data["data"][0]["id"]
 
 
-async def receive_chat_messages(channel_name):
-    token = await get_oauth_token(CLIENT_ID, CLIENT_SECRET)
-    channel_id = await get_channel_id(channel_name, token)
+# class ChatReceiver(QThread):
+#     message_received = pyqtSignal(str)
 
-    websocket_url = f"wss://irc-ws.chat.twitch.tv:443"
-    async with websockets.connect(websocket_url) as websocket:
-        # Send authentication and join channel
-        await websocket.send(f"PASS oauth:{token}")
-        await websocket.send(f"NICK justinfan123")
-        await websocket.send(f"JOIN #{channel_name}")
+#     async def receive_chat_messages(self, channel_name):
+#         token = await get_oauth_token(CLIENT_ID, CLIENT_SECRET)
+#         channel_id = await get_channel_id(channel_name, token)
 
-        # Receive messages
+#         websocket_url = f"wss://irc-ws.chat.twitch.tv:443"
+#         async with websockets.connect(websocket_url) as websocket:
+#             # Send authentication and join channel
+#             await websocket.send(f"PASS oauth:{token}")
+#             await websocket.send(f"NICK justinfan123")  # for read-only
+#             await websocket.send(f"JOIN #{channel_name}")
+
+#             # Receive messages
+#             while True:
+#                 message = await websocket.recv()
+#                 self.message_received.emit(message)
+
+#     def run(self):
+#         asyncio.run(self.receive_chat_messages(CHANNEL_NAME))
+
+
+class ChatReceiver(QThread):
+    message_received = pyqtSignal(str)
+
+    async def receive_chat_messages(self, channel_name):
+        token = await get_oauth_token(CLIENT_ID, CLIENT_SECRET)
+        channel_id = await get_channel_id(channel_name, token)
+
+        websocket_url = f"wss://irc-ws.chat.twitch.tv:443"
+
+        # Keep reconnecting and receiving messages
         while True:
-            message = await websocket.recv()
-            print(f"Message: {message}")
+            try:
+                async with websockets.connect(websocket_url) as websocket:
+                    await websocket.send(f"PASS oauth:{token}")
+                    await websocket.send(f"NICK justinfan123") # for read-only
+                    await websocket.send(f"JOIN #{channel_name}")
+
+                    while True:
+                        message = await websocket.recv()
+                        messages_queue.append(message)
+                        self.message_received.emit(message)
+            except Exception as e:
+                print(f"WebSocket Error: {e}")
+                print("Reconnecting...")
+                await asyncio.sleep(5)
+
+    def run(self):
+        asyncio.run(self.receive_chat_messages(CHANNEL_NAME))
+
+
+class ChatWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("Chat")
+
+        self.layout = QVBoxLayout()
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setReadOnly(True)
+        self.layout.addWidget(self.text_edit)
+
+        self.setLayout(self.layout)
+
+    @pyqtSlot(str)
+    def update_chat(self, message):
+        self.text_edit.append(message)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    chat_window = ChatWindow()
+    chat_window.show()
+
+    chat_receiver = ChatReceiver()
+    chat_receiver.message_received.connect(chat_window.update_chat)
+    chat_receiver.start()
+
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    asyncio.run(receive_chat_messages(CHANNEL_NAME))
+    main()
